22f5e981603a73f1054913172dfb669e9e612e2b
1767250 <1767250@li-h014-0t24>
2018-04-19 14:52:03 -0400

    Merge origin/master
    Conflicts:
    	src/tp2.java

---
+++ src/Mineral.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1767250
+ */
+public abstract class Mineral extends Specimen {
+
+    /**
+     *
+     * @param nom
+     * @param couleur
+     * @param taille
+     * @param dateObservation
+     * @param perso
+     */
+    public Mineral(String nom, String couleur, double taille, String dateObservation, Personne perso) {
+        super(nom,couleur,taille,dateObservation,perso);
+    }
+
+}
